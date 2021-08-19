72b68391e2c83181798922cdf8687a8cc2546a6a
lichen <lichen35@sfmail.sf-express.com>
2021-08-19 16:54:52 +0800

    添加storage的include和exclude

--- src/plugins/storage.ts
+++ src/plugins/storage.ts
@@ -10,31 +10,55 @@ interface Storage {
 interface StorageConfig {
   storageKey?: string;
   storage?: Storage;
-}
+  include?: string[];
+  exclude?: string[]
+};
 
 const session = {
   set(key: string, v: object) {
     sessionStorage.setItem(key, JSON.stringify(v));
   },
-  
+
   get(key: string) {
     return JSON.parse(sessionStorage.getItem(key) || '{}');
-  }
-}
-
-export const createStoragePlugin = (config?: StorageConfig): Plugin => {
-  const { storageKey = defaultStorageToken, storage = session } = config || {};
-  return {
-    preloadState: storage.get(storageKey),
-    enhancers: (next) => (...args) => {
-      const store = next(...args);
-      store.subscribe(() => {
-        storage.set(storageKey, store.getState());
-      });
-      return store;
-    }
-  };
+  },
 };
 
+export function createStoragePlugin(config: StorageConfig): Plugin {
+  const {
+    storageKey = defaultStorageToken,
+    storage = session,
+    include = [],
+    exclude = [],
+  } = config || {};
+  return {
+    preloadState: storage.get(storageKey),
+    enhancers: next => (...args) => {
+      const store = next(...args);
+      store.subscribe(() => {
+        const state: any = store.getState();
+        
+        if (include && include.length) {
+          const cache: any = {};
+          include.forEach(key => cache[key] = state[key])
+          storage.set(storageKey, cache);
+          return;
+        }
+
+        if (exclude && exclude.length) {
+          const cache: any = {};
+          Object.keys(state).forEach(key => {
+            if (exclude.indexOf(key) === -1) {
+              cache[key] = state[key];
+            }
+          });
+          storage.set(storageKey, cache);
+        }
+      });
+      return store;
+    },
+  };
+}
+
 /** 将store数据缓存到本地的插件 */
-export const storagePlugin = createStoragePlugin();
+export const storagePlugin = createStoragePlugin({exclude: []});
